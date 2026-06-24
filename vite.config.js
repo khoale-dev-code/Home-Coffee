cc23c1a06988160c4378deff9d22fb6e73981ebf
khoale-dev-code <96451842+khoale-dev-code@users.noreply.github.com.>
2026-06-24 16:04:20 +0700

    Fix Vite Tailwind Safari compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    target: ["es2020", "safari16.4"],
+    cssTarget: "safari16.4",
+    chunkSizeWarningLimit: 1200,
+  },
 });
